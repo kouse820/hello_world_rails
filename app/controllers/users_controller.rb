e9b6f813945b6081bb2eba32ba62846829c76501
竹ヶ原光匡 <>
2026-04-28 18:40:17 +0900

    CRUD処理の実装 Create, Update, Delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,47 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def index
     @users = User.all
 
     render json: @users
   end
+  def show
+  end
+
+  def create
+    # インスタンスを　model　から作成する
+    # @user = User.new(
+    #   name: params[:name],
+    #   account: params[:account],
+    #   email: params[:email],
+    # )
+
+    @user = User.new(user_params)
+    #　インスタンスを　DB　に保存する
+    @user.save!
+    #　ｊson　として値を返す
+    render :show
+  end
+
+  def update
+    #　探してきたレコードを更新する
+    @user.update!(user_params)
+    # json　として値を返す
+    render :show
+  end
+  def destroy
+    #　探してきたレコードを削除する
+     @user.destroy
+    #　ｊson　として値を返す
+    render :show
+  end
+
+ private
+   def user_params
+    params.require(:user).permit(:name, :account, :email)
+   end
+   def set_user
+    @user = User.find(params[:id])
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :articles
-  get 'users/index'
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  get 'homes/index'
 end
